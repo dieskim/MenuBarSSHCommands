77c4aa6b30a728e6b15ab21e34274a26319a58db
Dieskim <dieskim@julula.com>
2023-07-18 21:29:59 +0700

    Initial Commit

---
+++ MenuBarSSHCommands/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MenuBarSSHCommands
+//
+//  Created by Dieskim on 7/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MenuBarSSHCommands/MenuBarSSHCommandsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarSSHCommandsApp.swift
+//  MenuBarSSHCommands
+//
+//  Created by Dieskim on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarSSHCommandsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
